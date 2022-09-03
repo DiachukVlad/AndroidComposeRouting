f677cebcc1799028a68bf5ebebcb9f17efa0bc34
Vladyslav Diachuk <vdiachuk@fb.com>
2022-09-03 12:59:04 +0200

    publishing

--- routing/build.gradle.kts
+++ routing/build.gradle.kts
@@ -62,3 +62,28 @@ dependencies {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "16"
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.diachuk.routing"
+                artifactId = "android-compose-routing"
+                version = "1.0"
+                artifact("$buildDir/outputs/aar/routing-release.aar")
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+
+            url = uri("https://maven.pkg.github.com/DiachukVlad/AndroidComposeRouting") // Github Package
+            credentials {
+                //Fetch these details from the properties file or from Environment variables
+                username = project.properties.get("gpr.usr") as String? ?: System.getenv("GPR_USER")
+                password = project.properties.get("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+            }
+        }
+    }
+}
